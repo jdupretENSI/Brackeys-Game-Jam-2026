d7363fd83e3e0b23c2c9ff9935aa049cc164043a
Attom777 <attom.geek@gmail.com>
2026-02-21 14:52:45 +0100

    feat: implement main menu
    Design Layout : Create Canvas Scaler + Add Main Menu Panel + Create Background Image
    Core Buttons : Implemente Scene transitions

---
+++ Assets/_Branches/Neo/MainMenuManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("DreamSheep_V0.1");
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+}
