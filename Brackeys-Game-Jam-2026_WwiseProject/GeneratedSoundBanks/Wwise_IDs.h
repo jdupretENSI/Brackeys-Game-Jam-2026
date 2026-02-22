ec90b73823af65bb6900b2419f16ff79602b76b4
nathan-granes <contact.nathangranes@gmail.com>
2026-02-22 04:11:02 +0100

    Added Credits Theme to Wwise

--- Brackeys-Game-Jam-2026_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ Brackeys-Game-Jam-2026_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_CREDITSTHEME = 3468176687U;
         static const AkUniqueID PLAY_GAME_MUSIC = 4141037488U;
         static const AkUniqueID PLAY_ITEM_POP = 3233440943U;
         static const AkUniqueID PLAY_MAINMENU = 3738780720U;
@@ -25,6 +26,7 @@ namespace AK
         static const AkUniqueID PLAY_UI_MENU_DECLINE = 1047153021U;
         static const AkUniqueID PLAY_WOLF_BITE = 3251744751U;
         static const AkUniqueID PLAY_WOLF_IDLE_LOOP = 399082132U;
+        static const AkUniqueID STOP_ALL_MUS = 451295089U;
         static const AkUniqueID STOP_ALL_SFX = 971107033U;
         static const AkUniqueID STOP_MAINMENU = 890527358U;
     } // namespace EVENTS
